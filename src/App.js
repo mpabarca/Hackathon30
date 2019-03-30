e5dcc6b9cc32b9e28356a7cf7e8d763114b45532
mpabarca <mpabarca@uc.cl>
2019-03-30 17:28:44 -0300

    adecuando pagina gloria

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Start from './component-start/Start';
-import Login from './component-login/Login';
-import Navbar from './component-navbar/Navbar';
-import Desafios from './component-desafios/Desafios';
 import { Link } from 'react-router-dom';
-import Premios from './component-premios/Premios';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Premios/>        
+      <div className="App">    
       </div>
     )
   }
