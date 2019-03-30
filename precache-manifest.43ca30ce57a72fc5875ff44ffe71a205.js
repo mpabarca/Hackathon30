8802da65356aedf547033fe7cd287e26681e45e0
mpabarca <mpabarca@uc.cl>
2019-03-30 17:35:40 -0300

    Updates

---
+++ precache-manifest.43ca30ce57a72fc5875ff44ffe71a205.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f597993966abe60294f9",
+    "url": "/Hackathon30/static/css/main.daa32e91.chunk.css"
+  },
+  {
+    "revision": "f597993966abe60294f9",
+    "url": "/Hackathon30/static/js/main.7a22bdb3.chunk.js"
+  },
+  {
+    "revision": "54b907af71fa6aa801d9",
+    "url": "/Hackathon30/static/js/runtime~main.fe75d701.js"
+  },
+  {
+    "revision": "42d6991ec15bc91350df",
+    "url": "/Hackathon30/static/css/2.409fa4c6.chunk.css"
+  },
+  {
+    "revision": "42d6991ec15bc91350df",
+    "url": "/Hackathon30/static/js/2.dffa9d39.chunk.js"
+  },
+  {
+    "revision": "8cd334d85453b02d560ad8ee99bdcfe8",
+    "url": "/Hackathon30/static/media/logo.8cd334d8.png"
+  },
+  {
+    "revision": "b3e30bdb2e34a96c164e4cb52195b07e",
+    "url": "/Hackathon30/static/media/profile.b3e30bdb.png"
+  },
+  {
+    "revision": "be6b8ae930ca4cd9a05d16dae6d3deb7",
+    "url": "/Hackathon30/static/media/profile2.be6b8ae9.png"
+  },
+  {
+    "revision": "69f7db939f3934b37e32cf61190220eb",
+    "url": "/Hackathon30/static/media/steven.69f7db93.png"
+  },
+  {
+    "revision": "5f5a74f7ab62e5048a20fc3ce4f8f2cd",
+    "url": "/Hackathon30/static/media/plus.5f5a74f7.png"
+  },
+  {
+    "revision": "4c7507bf9726a7b648f685459fa32a64",
+    "url": "/Hackathon30/static/media/user.4c7507bf.png"
+  },
+  {
+    "revision": "b7fed01a060d94ed1376f91e136f05db",
+    "url": "/Hackathon30/static/media/arroba.b7fed01a.png"
+  },
+  {
+    "revision": "d8c7dc59ed4a26e686b3f8d5034298f5",
+    "url": "/Hackathon30/static/media/connie.d8c7dc59.png"
+  },
+  {
+    "revision": "dc17043b7097f4f85845c00788f9bb66",
+    "url": "/Hackathon30/index.html"
+  }
+];
