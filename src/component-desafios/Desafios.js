a5d0bf500f176e1451f40e68f0008835e8713140
mpabarca <mpabarca@uc.cl>
2019-03-30 15:25:15 -0300

    se crea componente desafios

---
+++ src/component-desafios/Desafios.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './desafios.css';
+
+class Desafios extends Component{
+
+    render(){
+        return(
+            <Container>
+                
+            </Container>
+
+        )
+    }
+}
+
+export default Desafios;
