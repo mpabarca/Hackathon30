a01c7fc1d8c34e49a9bf6010b8a5235ae808353a
mpabarca <mpabarca@uc.cl>
2019-03-30 09:59:14 -0300

    pequeños cambios

---
+++ src/component-login/Login.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import fire from '../components-firebase/Fire';
+import './login.css';
+
+class Login extends Component{
+
+    render(){
+        return(
+
+        )
+    }
+}
