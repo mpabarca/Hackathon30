dfc8bfcb66242861f4b627b3757eed775c14a091
mpabarca <mpabarca@uc.cl>
2019-03-30 10:34:24 -0300

    se hace maquetado pagina start

--- src/component-login/Start.js
+++ src/component-login/Start.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import './start.css';
 import logo from '../img/logo.png';
-import { Container, Row} from 'react-bootstrap';
+import { Container, Row, Button} from 'react-bootstrap';
 
 class Start extends Component{
 
     render(){
         return(
-            <Container>
-                <Row>
-                </Row>
-                <Row>
-                </Row>
+            <Container id="start">
+                <Row id="start-logo"><img src={logo} id="app-logo"/></Row>
+                <div id="start-button">
+                    <Row id="start-login"><Button variant="primary">Inicia Sesión</Button></Row>
+                    <Row id="start-team"><Button variant="secondary">Crear nuevo equipo</Button></Row>
+                </div>
             </Container>
 
         )
